37e413ed8aad3f84bd3dc047f97d769e3ef6c950
Matias Baez <matiasbaez2512@gmail.com>
2024-06-09 14:31:31 -0400

    feat: add domain errors layer

---
+++ src/domain/index.ts
@@ -0,0 +1,12 @@
+
+export * from "./errors/custom.error";
+
+export * from "./entities/user.entity";
+export * from "./entities/product.entity";
+export * from "./entities/category.entity";
+
+export * from "./dtos/auth/login-user.dto";
+export * from "./dtos/shared/pagination.dto";
+export * from "./dtos/auth/register-user.dto";
+export * from "./dtos/product/create-product.dto";
+export * from "./dtos/category/create-category.dto";
