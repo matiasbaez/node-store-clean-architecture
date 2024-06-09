a20cb6d76be3f5c9b285a31e800bf0e025c9d0ed
Matias Baez <matiasbaez2512@gmail.com>
2024-06-09 14:34:01 -0400

    feat: configure app

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { envs } from './config/envs';
+import { MongoDataBase } from './data';
+import { AppRoutes } from './presentation/routes';
+import { Server } from './presentation/server';
+
+
+(async()=> {
+  main();
+})();
+
+
+async function main() {
+
+  await MongoDataBase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    dbUrl: envs.MONGO_URL
+  });
+
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
